ac7344354e2882ccfae50b36ed1e225ad6427ec7
KIJones <keithjones001@yahoo.com>
2018-05-21 19:14:40 +0100

    Initial commit

---
+++ WebCalculator/Scripts/CalculatorLibrary.js
@@ -0,0 +1,46 @@
+﻿var txtInput;
+var txtResult;
+function initialize() {
+    for (var i = 0; i < 10; i++) {
+        document.getElementById('btn'+i).addEventListener('click', numberClick, false);
+    }
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+
+    document.getElementById('btnPlus').addEventListener('click', plusClick, false);
+    document.getElementById('btnMinus').addEventListener('click', minusClick, false);
+    document.getElementById('btnClearEntry').addEventListener('click', clearEntryClick, false);
+    document.getElementById('btnClear').addEventListener('click', clearClick, false);
+    txtInput.value = "0";
+    txtResult.value = "0";
+    //clear(); * does not seem to work
+    
+
+
+}
+
+function numberClick() {
+    txtInput.value = txtInput.value == '0' ? this.innerText : txtInput.value + this.innerText;
+    //txtInput.value = this.innerText;
+}
+
+function plusClick() {
+    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+    //txtInput.clearEntry();
+    txtInput.value = "0";
+}
+
+function minusClick(){
+    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+    //txtInput.clearEntry();
+    txtInput.value = "0";
+}
+
+function clearEntryClick() {
+    txtInput.value = "0";
+}
+
+function clearClick() {
+    txtInput.value = "0";
+    txtResult.value = "0";
+}
---
+++ WebCalculator/Scripts/tests.js
@@ -0,0 +1,76 @@
+﻿module('Calculator Test Suite', { setup: function () { initialize(); } });
+
+test("Initialize Test", function () {
+    expect(2);
+    //txtInput.value = '';
+    //txtResult.value = '';
+    var expected = "0";
+    equal(txtInput.value, expected, "Expected value: " + expected + " Actual value: " + txtInput.value);
+    equal(txtResult.value, expected, "Expected value: " + expected + " Actual value: " + txtResult.value);
+})
+
+
+
+
+
+
+test("Btn Click Test", function () {
+    expect(10);
+    for (var count = 0; count <= 9; count++) {
+        var btn = document.getElementById('btn'+count);
+        QUnit.triggerEvent(btn, "click");
+        var result = txtInput.value[txtInput.value.length-1];
+        var expected = String(count);
+        equal(result, expected, 'Expected value: ' + expected + ' Actual value: ' + result);
+        
+    }
+
+});
+
+test("Add Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    var btnPlus = document.getElementById('btnPlus');
+    QUnit.triggerEvent(btnPlus, "click");
+    var expected = '30';
+    equal(txtResult.value, expected, "Expected value: " + expected + " Actual value: " + txtResult.value);
+    expected = "0";
+    equal(txtInput.value, expected, "Expected value: " + expected + " Actual value: " + txtInput.value);
+
+    
+});
+
+test("Subtract Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    var btnMinus = document.getElementById('btnMinus');
+    QUnit.triggerEvent(btnMinus, 'click');
+    var expected = '10';
+    equal(txtResult.value, expected, "Expected value: " + expected + " Actual value: " + txtResult.value);
+    expected = "0";
+    equal(txtInput.value, expected, "Expected value: " + expected + " Actual value: " + txtInput.value);
+})
+
+test("Clear Entry", function () {
+    expect(1)
+    txtInput.value = '10';
+    var btnClearEntry = document.getElementById('btnClearEntry');
+    QUnit.triggerEvent(btnClearEntry, 'click');
+    var expected = "0";
+    equal(txtInput.value, expected, "Expected value: " + expected + " Actual value: " + txtInput.value);
+});
+
+test("Clear Test", function () {
+    expect(2)
+    txtInput.value = "10";
+    txtResult.value = "10";
+    var expected = "0";
+    //var btnClear = document.getElementById('btnClear');
+    QUnit.triggerEvent(btnClear, 'click');
+    equal(txtInput.value, expected, "Expected: " + expected + " Actual: " + txtInput.value);
+    equal(txtResult.value, expected, "Expected: " + expected + " Actual: " + txtResult.value);
+ 
+
+})
